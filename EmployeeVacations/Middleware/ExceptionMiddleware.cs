d4685e9bb703d846c26a96fccc0230ecf94f5a15
ahrawashdeh <rawshdeh51@gmail.com>
2023-03-06 11:53:37 +0300

    Add Middleware

---
+++ EmployeeVacations/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using EmployeeVacations.Api.Error;
+using System.Net;
+using System.Text.Json;
+
+namespace EmployeeVacations.Api.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IWebHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, IWebHostEnvironment env) 
+        {
+            _next = next;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context) 
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex) 
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException(context.Response.StatusCode, ex.Message, "Internal Server Error");
+
+                var json = JsonSerializer.Serialize(response);
+
+                await context.Response.WriteAsync(json);
+
+            }
+        }
+
+
+    }
+}
